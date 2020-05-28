cab44e34bf3dcdb6530d3697c0b8db9b3cacfd24
Samantha Wabi <swfullpotential@gmail.com>
2020-05-28 21:11:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,11 @@
 
 def my_collect(array)
   
-  modified_array = []
   index = 0 
-  
   while index < array.length 
-    modified_array << yeild(array[index])
-  index += 1 
   
-  end
-  modified_array
+  
+    index += 1 
+end
 end
 
